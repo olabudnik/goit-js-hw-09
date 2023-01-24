f00d8ca352a7e1d22ba88b8e2b7d6553aec8ff69
olabudnik <olyabudnik98@gmail.com>
2023-01-24 21:40:45 +0100

    homework

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import {Notify} from "notiflix";
+
+const form = document.querySelector(".form");
+
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { amount, delay, step} = event.target.elements;
+
+ let delayValue = +delay.value; 
+
+  for (let i = 0; i < +amount.value; i += 1) {
+    createPromise(i+1, delayValue).then(({position, delay}) => {
+     Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  delayValue += +step.value; 
+  };
+  event.target.reset();
+}; 
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+ const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+ resolve({position, delay});
   } else {
-    // Reject
+    reject({position, delay});
   }
+  }, delay); 
+  });
 }
+
+
